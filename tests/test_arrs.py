9f5d136abca0ac3f9341d382b8cb69e4d034bc79
dashuta <it.dashutka@gmail.com>
2023-06-21 01:08:31 +0300

    fixcov

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,6 +7,15 @@ def test_get():
     assert arrs.get([1, 2, 3], -2, "test") == "test"
 
 
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5], -4, -1) == [2, 3, 4]
+    assert arrs.my_slice([], -3, -1) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], -7, 2) == [1, 2]
+
+
+
+
+
